bbbf0949c36ba02a2e0f031d09ec57d706a2f25f
sadoon <muhammadsadoonsohail786@gmail.com>
2025-06-17 21:10:53 +0500

    add more page section like 4,5,6

--- app.js
+++ app.js
@@ -17,3 +17,22 @@ function next_btn() {
     handlerParentDev.appendChild(firstItem)
     arr.push(firstItem)
 }
+var items_2 = document.querySelectorAll(".commit_boxes ul li");
+var handlerParentDev_2 = document.querySelector(".commit_boxes ul")
+var arr_2 = [];
+items_2.forEach((v) => {
+    arr_2.push(v)
+})
+console.log(arr_2)
+function pervious_btn_2() {
+    var lastItem = arr_2.pop()
+    handlerParentDev_2.removeChild(lastItem)
+    handlerParentDev_2.appendChild(lastItem)
+    arr_2.unshift(lastItem)
+}
+function next_btn_2() {
+    var firstItem = arr_2.shift()
+    handlerParentDev_2.removeChild(firstItem)
+    handlerParentDev_2.appendChild(firstItem)
+    arr_2.push(firstItem)
+}
